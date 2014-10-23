cd1ae7d1454fed4cdb3a5bc2fee6d4b55630c2ef
melaniemccall <mmccall8@gatech.edu>
2014-10-23 08:43:57 -0400

    Javadocs added to Character

--- src/spaceTrader/Character.java
+++ src/spaceTrader/Character.java
@@ -14,6 +14,8 @@ package spaceTrader;
 
 //name, 4 skill points, difficulty level, 
 
+
+
 public class Character {
      
     private String name = "";
@@ -38,52 +40,122 @@ public class Character {
     }
     
     //GETTERS
-    
+
+    /**
+    *Returns name of character
+    *@return name as a String
+    *
+    */    
     public String getName(){
         return name;
     }
     
+    /**
+    *Returns pilot points of character
+    *@return points as an int
+    *
+    */
+
     public int getPilotPoints(){
         return pilotPoints;
     }
-    
+
+    /**
+    *Returns fighter points of character
+    *@return points as an int
+    *
+    */
+
     public int getFighterPoints(){
         return fighterPoints;
     }
     
+    /**
+    *Returns trader points of character
+    *@return points as an int
+    *
+    */
+
     public int getTraderPoints(){
         return traderPoints;
     }
     
+    /**
+    *Returns engineer points of character
+    *@return points as an int
+    *
+    */
+
     public int getEngineerPoints(){
         return engineerPoints;
     }
     
+    /**
+    *Returns difficulty level of character
+    *@return level as a String
+    *
+    */
     public String getDifficultyLevel(){
         return difficultyLevel;
     }
     
     // SETTERS
+    
+    /**
+    *Sets name of the character
+    *@param desired name as a string
+    *
+    */
+    
     public void setName(String name){
         this.name = name; 
     }
     
+    /**
+    *Sets fighter points of the character
+    *@param points as an int
+    *
+    */
+
     public void setFighterPoints(int fighterPoints){
         this.fighterPoints = fighterPoints;
     }
     
+    /**
+    *Sets trader points of the character
+    *@param points as an int
+    *
+    */
+
     public void setTraderPoints(int traderPoints){
         this.traderPoints = traderPoints;
     }
     
+    /**
+    *Sets engineer points of the character
+    *@param points as an int
+    *
+    */
+
     public void setEngineerPoints(int engineerPoints){
         this.engineerPoints = engineerPoints;
     }
     
+    /**
+    *Sets pilot points of the character
+    *@param points as an int
+    *
+    */
+
     public void setPilotPoints (int pilotPoints){
         this.pilotPoints = pilotPoints;
     }
     
+    /**
+    *Returns character name and attributes in a string
+    *@return String
+    *
+    */
     public String toString()
     {
         return "Name: " + name + "; Pilot Points: " + pilotPoints + "; Fighter Points: " + fighterPoints + "; Trader Points: " 
